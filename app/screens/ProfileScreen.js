578a576ff590a96b3fcd3140b459c46c3efe3eb2
Keuz <kevin.gaming@hotmail.com>
2019-05-23 11:52:41 +0200

    PP, header, colors

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -7,21 +7,21 @@ export default class ProfileScreen extends React.Component {
         return (
             <SafeAreaView style={styles.container}>
                 <View style={styles.header}>
-                    <Text style={styles.description}>Nicolai Vatne</Text>
-                    <Text style={styles.description}>Student</Text>
-                    <Text style={styles.description}>Student at Kristiania University College</Text>
+                    <Text style={styles.name}>Mikke Mus</Text>
+                    <Text style={styles.school}> - Høyskolen Kristiania - </Text>
 
                 </View>
+                <Image style={styles.avatar} source={{uri: 'https://scontent.fosl1-1.fna.fbcdn.net/v/t1.0-9/18697997_10154623209420949_6811334622727457880_n.jpg?_nc_cat=105&_nc_ht=scontent.fosl1-1.fna&oh=be41132c36528b70937cd33ef281db85&oe=5C9DA004'}}/>
                 <View style={styles.body}>
                     <TouchableOpacity
                         style={styles.buttonContainer}
                         onPress={()=> this.props.navigation.navigate('ProfileEdit')}>
-                        <Text>Edit my Profile</Text>
+                        <Text style={styles.editors}>Edit my Profile</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         style={styles.buttonContainer}
                         onPress={()=> this.props.navigation.navigate('HomeScreen')}>
-                        <Text>Edit my Preferences</Text>
+                        <Text style={styles.editors}>Edit my Preferences</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
@@ -30,56 +30,67 @@ export default class ProfileScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    header: {
-        backgroundColor: "#00bfff",
-        height: 240,
+    header:{
+        backgroundColor: '#455a64',
+        height:250,
+        alignItems: 'center',
     },
     avatar: {
-        width: 130,
-        height: 130,
+        width: 135,
+        height: 135,
         borderRadius: 63,
         borderWidth: 4,
-        borderColor: "white",
-        marginBottom: 10,
-        alignSelf: 'center',
+        borderColor: 'transparent',
+        marginBottom:10,
+        alignSelf:'center',
         position: 'absolute',
-        marginTop: 130
+        marginTop: 40,
     },
     name: {
-        fontSize: 22,
-        color: "#FFFFFF",
-        fontWeight: '600',
-        marginLeft: 45,
+        marginTop: 200,
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: 'bold',
     },
-    body: {
-        marginTop: 40,
+    school: {
+        fontSize: 14,
+        color: '#0394c0',
+        fontWeight: '300',
+        fontStyle: 'italic'
+    },
+    editors: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    body:{
+        marginTop:40,
     },
     bodyContent: {
         flex: 1,
         alignItems: 'center',
-        padding: 30,
+        padding:30,
     },
-    info: {
-        fontSize: 16,
+    info:{
+        fontSize:16,
         color: "#00BFFF",
-        marginTop: 10
+        marginTop:10
     },
-    description: {
-        fontSize: 16,
+    description:{
+        fontSize:16,
         color: "#696969",
         marginTop: 35,
         textAlign: 'center'
     },
     buttonContainer: {
-        marginTop: 50,
-        height: 45,
-        left: 75,
+        marginTop:50,
+        height:45,
+        left: 75 ,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 20,
-        width: 250,
-        borderRadius: 30,
-        backgroundColor: "#00BFFF",
+        marginBottom:20,
+        width:250,
+        borderRadius:30,
+        backgroundColor: '#455a64',
     },
 });
