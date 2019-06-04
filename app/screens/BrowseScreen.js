300fb539e8f5c79258d5b5fba9a8173eff96bdc8
Nicolai Vatne <nicolaivatne@Nicolais-MacBook-Pro.local>
2019-06-04 19:05:37 +0200

    Something

--- app/screens/BrowseScreen.js
+++ app/screens/BrowseScreen.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react"
 
 export default class BrowseScreen extends Component {
+
+  componentDidMount() {
+    this.listener = firebase.firestore().collection("events")
+        .orderBy("timestamp", "desc")
+        .limit(20)
+        .onSnapshot(qss => {
+          const events = qss.docs
+              .filter(doc => doc.get("timestamp").toDate() >= Date.now())
+              .reverse()
+          this.setState(events)
+        }, console.log)
+  }
+  componentWillMount() {
+    this.listener && this.listener()
+  }
+  getItems = () => this.state.events.map(doc => ({
+    component: Room,
+    props: {doc, onPress: () => this.props.navigation.navigate("HomeChat",{"events/" + doc.id + "/chat"})},
+  }))
+
   render() {
-    return console.log("something")
+    return console.log("Rendering with length " + this.state.events.length)
+
   }
 }
 
